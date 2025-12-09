84ca7dab9c07d3993bc5478afb183bf10163dc05
VicksMP <virginia.mango@gmail.com>
2025-12-09 16:25:18 -0300

    Add Prisma integration and update project structure
    - Added Prisma as a dependency and created a Prisma configuration file.
    - Introduced a new schema for the User model in `prisma/schema.prisma`.
    - Updated `.gitignore` to exclude generated Prisma files.
    - Modified `tsconfig.json` to include new path mappings for Prisma.
    - Created a new `TestComponent` for testing purposes in the app.
    - Refactored the main page to utilize the new `TestComponent` and removed default content.
    - Added a Prisma client instance in `app/lib/prisma.ts` for database interactions.

---
+++ app/components/TestComponent.jsx
@@ -0,0 +1,7 @@
+export default function TestComponent() {
+    return (
+        <div className="bg-green-950 flex items-center justify-center w-full h-full">
+            This is a test
+        </div>
+    )
+}
